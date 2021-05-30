13b60ea0fb78436ab97f466e011a411a541a2182
Haydin Mora <50211083+haydinmora09@users.noreply.github.com>
2021-05-29 23:47:53 -0500

    project init

---
+++ src/test/java/co/edu/usbbog/bdd/ApiBancoBogotaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoBogotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
